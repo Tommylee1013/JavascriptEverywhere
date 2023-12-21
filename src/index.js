0b5f0442156370831545e7c364838abc7b2e47ed
Tommy Lee <junghun1013@icloud.com>
2023-12-21 15:04:54 +0900

    Saving

--- src/index.js
+++ src/index.js
@@ -3,16 +3,52 @@
 
 const express = require('express');
 const { ApolloServer, gql} = require('apollo-server-express')
+
+let notes = [
+    { id : '1', content : 'This is a note', author : 'Adam Scott' },
+    { id : '2', content : 'This is another note', author : 'Harlow Everly' },
+    { id : '3', content : 'Oh hey look, another note!', author : 'Riley Harrison' }
+]
+
 const port = process.env.PORT || 4000;
 const typeDefs = gql`
     type Query {
-        hello : String
+        hello : String!
+        notes : [Note!]!
+        note(id : ID!) : Note!
+    }
+    
+    type Note {
+        id : ID!
+        content : String!
+        author : String!
+    }
+    
+    type Mutation {
+        newNote(content : String!) : Note!
     }`;
+
 const resolvers = {
     Query : {
-        hello : () => 'Hello world!'
+        hello : () => 'Hello world!',
+        notes : () => notes,
+        note : (parent, args) => {
+            return notes.find(note => note.id === args.id);
+        }
+    },
+    Mutation : {
+        newNote: (parent, args) => {
+            let noteValue = {
+                id: String(notes.length + 1),
+                content: args.content,
+                author: 'Adam Scott'
+            };
+            notes.push(noteValue);
+            return noteValue;
+        }
     }
 };
+
 const app = express();
 const server = new ApolloServer({typeDefs, resolvers});
 async function startServer() {
